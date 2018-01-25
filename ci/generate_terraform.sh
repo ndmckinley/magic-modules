5ed1ca5c5c8863e9f51e4cf36f35d0a8df30c523
Nathan McKinley <nmckinley@google.com>
2018-01-25 21:23:40 +0000

    Needs to output to directory!

--- ci/generate_terraform.sh
+++ ci/generate_terraform.sh
@@ -23,9 +23,13 @@ cd "${WORKDIR}/magic-modules"
 bundle install
 bundle exec ruby compiler.rb -p products/compute -e terraform -o "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google/google/" -t BackendBucket
 
-cd "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
+cd "build/terraform"
 git add -A
 git config user.email "nmckinley@google.com"
 git config user.name "Magic Module Bot"
 git commit -m "magic modules change happened here"
-git checkout -b magic_modules
+git checkout -B magic_modules
+
+cd "../../"
+
+cp -r ./ "${WORKDIR}/mm-output/"
