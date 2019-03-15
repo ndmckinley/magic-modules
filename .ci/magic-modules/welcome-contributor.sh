c2471eb5bc055f82bf2c981821f5225f565b94f9
Nathan McKinley <nmckinley@google.com>
2019-03-15 16:49:14 -0700

    eh this is worse

--- .ci/magic-modules/welcome-contributor.sh
+++ .ci/magic-modules/welcome-contributor.sh
@@ -5,4 +5,4 @@ Hello!  I am a robot who works on Magic Modules PRs.
 I have detected that you are nathan doing a test of things.  Thanks for your contribution!  This problem is your fault in the first place.
 EOF
 
-shuf -n 1 <(printf "ndmckinley\nrambleraptor\nchrisst\nrileykarson\nSirGitsalot\nslevenick") > comment/assignee
+shuf -n 1 <(printf "ndmckinley") > comment/assignee
