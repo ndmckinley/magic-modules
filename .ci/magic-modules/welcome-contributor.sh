88da593bcda85a0ade4e95f12c6fc46acb4fd5a1
Nathan McKinley <nmckinley@google.com>
2019-03-15 16:31:09 -0700

    oh boy do not submit this, boyo.

--- .ci/magic-modules/welcome-contributor.sh
+++ .ci/magic-modules/welcome-contributor.sh
@@ -2,9 +2,7 @@
 cat > comment/pr_comment << EOF
 Hello!  I am a robot who works on Magic Modules PRs.
 
-I have detected that you are a community contributor, so your PR will be assigned to someone with a commit-bit on this repo for initial review.  They will authorize it to run through our CI pipeline, which will generate downstream PRs.
-
-Thanks for your contribution!  A human will be with you soon.
+I have detected that you are nathan doing a test of things.  Thanks for your contribution!  This problem is your fault in the first place.
 EOF
 
 shuf -n 1 <(printf "ndmckinley\nrambleraptor\nchrisst\nrileykarson\nSirGitsalot\nslevenick") > comment/assignee
